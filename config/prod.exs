e0085965707dfc3400227018724532a178f34b4b
Sebastian Grebe <sebigrebe@googlemail.com>
2020-05-12 20:21:43 +0200

    ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :plantguru, PlantguruWeb.Endpoint,
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
